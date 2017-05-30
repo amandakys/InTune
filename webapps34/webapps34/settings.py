06103c1178e51197ffd2aa584987c541bb0944d8
Linna Wang <lw4715@ic.ac.uk>
2017-05-30 18:04:36 +0100

    user_home template: display song names

--- webapps34/webapps34/settings.py
+++ webapps34/webapps34/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'intune',
+    'intune.apps.IntuneConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
