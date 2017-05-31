822eab26230f1f1b79319e2f5361c7e901948042
Si Wei Tan <si.tan15@imperial.ac.uk>
2017-05-31 02:10:26 +0100

    Expanded stub methods in IR

--- webapps34/intune/ir.py
+++ webapps34/intune/ir.py
@@ -1,17 +1,40 @@
-# Abstract Class
+from intune.irdefs import Accidental
+
+
 class Note:
-    # TODO: Abstract class / Interface
-    pass
+    # TODO: Refactor to ABC class
+
+    def __init__(self, duration):
+        self.duration = duration
+
+    def modulate(self, semitones, keysig):
+        pass
 
 
-class RestNote:
-    # TODO: Implements Note
-    pass
+class RestNote(Note):
+
+    def __init__(self, duration):
+        Note.__init__(self, duration)
 
 
-class RegNote:
-    # TODO: Implements Note
-    pass
+class RegNote(Note):
+
+    def __init__(self, duration, pitch, octave):
+        Note.__init__(self, duration)
+        self.pitch = pitch
+        self.octave = octave
+        self.accidental = Accidental.NONE
+
+    def modulate(self, semitones, keysig):
+        """
+        Changes the note by the number of semitones given
+        :param keysig: key signature used by this note
+        :param semitones: int (+/-) number of semitones to change
+        :return: self
+        """
+
+        # TODO: Do actual modulation
+        return self
 
 
 # || Segment Class Overview ||
@@ -35,15 +58,20 @@ class Segment:
 
     def changekey(self, newkey):
         self.keySig = newkey
+        # TODO: modulate all notes wrt new key
+        return self
 
     def changetimesig(self, newtimesig):
         self.timeSig = newtimesig
+        return self
 
     def addnote(self, index, note):
         self.notes.insert(index, note)
+        return self
 
     def delnote(self, index):
         self.notes.remove(index)
         # Rest placeholder
         self.notes.insert(index, RestNote())
+        return self
 
