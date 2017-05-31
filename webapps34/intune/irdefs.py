e926330c49e0767898f004535f242c43debe4ff2
Si Wei Tan <si.tan15@imperial.ac.uk>
2017-05-31 02:09:48 +0100

    Created irdefs.py containing enum types for IR

---
+++ webapps34/intune/irdefs.py
@@ -0,0 +1,40 @@
+from enum import IntEnum, Enum, auto
+
+
+class Pitch(IntEnum):
+    # Overview:
+    # 1. 12 semitones exists in an octave
+    # 2. Start at C
+    # More details:
+    # Refer to https://en.wikipedia.org/wiki/Piano_key_frequencies
+    C = 1
+    CSHARP = 2
+    DFLAT = 2
+    D = 3
+    DSHARP = 4
+    EFLAT = 4
+    E = 5
+    FFLAT = 5
+    ESHARP = 6
+    F = 6
+    FSHARP = 7
+    GFLAT = 7
+    G = 8
+    GSHARP = 9
+    AFLAT = 9
+    A = 10
+    ASHARP = 11
+    BFLAT = 11
+    B = 12
+    BSHARP = 1
+
+
+class Accidental(Enum):
+    # Overview:
+    # Accidentals are referenced only during rendering, pitch should reflect
+    # the true tone of that note
+    NONE = auto()
+    SHARP = auto()
+    DSHARP = auto()
+    FLAT = auto()
+    DFLAT = auto()
