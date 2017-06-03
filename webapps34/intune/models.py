86ac963a2658d9322bcdd77fbef80bec655476d8
Linna Wang <linna.wang15@imperial.ac.uk>
2017-06-03 17:58:13 +0100

    cleanup models

--- webapps34/intune/models.py
+++ webapps34/intune/models.py
@@ -1,6 +1,6 @@
-import select2.fields
 from django.db import models
 from django.contrib.auth.models import User
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -13,17 +13,6 @@ class Composition(models.Model):
     title = models.CharField(max_length=200)
     owner = models.ForeignKey(Profile, related_name='owner', on_delete=models.CASCADE)
     users = models.ManyToManyField(Profile, blank=True)
-
-    # users = select2.fields.ManyToManyField(Profile,
-    #     ajax=True,
-    #     search_field=lambda q: Q(user__icontains=q),
-    #     sort_field='position',
-    #     js_options={'quiet_millis': 200})
-
-    # users = select2.fields.ManyToManyField(Profile, through='SharedUsers', limit_choices_to=models.Q(active=True),
-    #                                ajax=True, search_field='name', overlay="Choose shared users...",
-    #                                js_options={'quiet_millis': 200, },
-    #                                on_delete=models.CASCADE)
     # data = models.BinaryField()
     data = models.CharField(max_length=10000, blank=True)
     lastEdit = models.DateTimeField(auto_now=True, auto_now_add=False)
