905c503cac9c3c1b310c9573cb214967bb323b4f
Si Wei Tan <swt14@ic.ac.uk>
2017-06-04 17:23:01 +0100

    vextab-renderer: reduced all var scopes

--- webapps34/intune/static/intune/vextab-renderer.js
+++ webapps34/intune/static/intune/vextab-renderer.js
@@ -1,34 +1,33 @@
-// Load VexTab module.
-vextab = VexTabDiv;
+$(function () {
+    var vt = VexTabDiv;
+    var VexTab = vt.VexTab;
+    var Artist = vt.Artist;
+    var Renderer = Vex.Flow.Renderer;
 
-$(function() {
-  VexTab = vextab.VexTab;
-  Artist = vextab.Artist;
-  Renderer = Vex.Flow.Renderer;
+    // Remove "vexflow.com" logo that is rendered by default
+    Artist.NOLOGO = true;
+    VexTab.DEBUG = false;
 
-  Artist.DEBUG = true;
-  VexTab.DEBUG = false;
+    // Create VexFlow Renderer from canvas element with id #canvas-to-load
+    var renderer = new Renderer($('#canvas-to-load')[0], Renderer.Backends.CANVAS);
 
-  // Create VexFlow Renderer from canvas element with id #canvas-to-load
-  renderer = new Renderer($('#canvas-to-load')[0], Renderer.Backends.CANVAS);
+    // Initialize VexTab artist and parser.
+    var artist = new Artist(10, 10, 600, {scale: 0.8});
+    var vextab = new VexTab(artist);
 
-  // Initialize VexTab artist and parser.
-  artist = new Artist(10, 10, 600, {scale: 0.8});
-  vextab = new VexTab(artist);
-
-  function render() {
-    try {
-      vextab.reset();
-      artist.reset();
-      vextab.parse($("#to-render").val());
-      artist.render(renderer);
-      $("#error").text("");
-    } catch (e) {
-      console.log(e);
-      $("#error").html(e.message.replace(/[\n]/g, '<br/>'));
+    function render() {
+        try {
+            vextab.reset();
+            artist.reset();
+            vextab.parse($("#to-render").val());
+            artist.render(renderer);
+            $("#error").text("");
+        } catch (e) {
+            console.log(e);
+            $("#error").html(e.message.replace(/[\n]/g, '<br/>'));
+        }
     }
-  }
 
-  $("#to-render").keyup(_.throttle(render, 250));
-  render();
+    $("#to-render").keyup(_.throttle(render, 250));
+    render();
 });
