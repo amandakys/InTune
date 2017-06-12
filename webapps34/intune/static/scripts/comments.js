eb7394587658ebd0e9c9441513da77d428aaeeee
Linna Wang <linna.wang15@imperial.ac.uk>
2017-06-12 17:13:23 +0100

    comments: add glyphicon, change order of comments to newest first and move form to top

--- webapps34/intune/static/scripts/comments.js
+++ webapps34/intune/static/scripts/comments.js
@@ -83,7 +83,7 @@ $(document).ready(function () {
         function display_new_comment(comment) {
             var comment_element = document.createElement("div");
             comment_element.setAttribute("class", "comment-element");
-            document.getElementById("comments").appendChild(comment_element);
+            document.getElementById("comments").prepend(comment_element);
 
             var name_col = document.createElement("div");
             name_col.setAttribute("class", "comment col-sm-8");
