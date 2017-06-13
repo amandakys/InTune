a162d5ebb12a2d37ad1f02800de59efe18aa309c
Linna Wang <linna.wang15@imperial.ac.uk>
2017-06-13 16:01:14 +0100

    chat notifications when new chat received (when chat dropdown is closed)

--- webapps34/intune/static/intune/chat.js
+++ webapps34/intune/static/intune/chat.js
@@ -4,6 +4,8 @@ $( document ).ready(function() {
     var user_id = $("div.roomdiv").attr("data-user-id");
     var socket = new WebSocket("ws://" + window.location.host + "/chat/" + room_id + "/");
 
+    var unread_chat = $("#unread_chats");
+
     socket.onmessage = function (e) {
         var data = JSON.parse(e.data);
         var msg_div = $(
@@ -14,6 +16,15 @@ $( document ).ready(function() {
 
         // auto scroll to bottom of chat
         $("#chats").scrollTop($("#chats")[0].scrollHeight);
+
+        // update unread_chat number if chat is not open
+        if (!$("#chat-dropdown").hasClass("open")) {
+            var current_unread = unread_chat.html();
+            if (current_unread) {
+                current_unread = parseInt(current_unread);
+            }
+            unread_chat.html(current_unread + 1);
+        }
     };
 
     socket.onopen = function () {
@@ -29,5 +40,13 @@ $( document ).ready(function() {
             event.preventDefault();
             $("#chat-msg").val("");
         });
-    }
+    };
+
+    $("#chat-dropdown-toggle").click(function() {
+        // set unread chat count to zero when chat is opened
+        if ($("#chat-dropdown").hasClass("open")) {
+            console.log("chat is opened");
+            unread_chat.html("");
+        }
+    });
 });
