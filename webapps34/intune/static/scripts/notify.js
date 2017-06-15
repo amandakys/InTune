36c9e5323412ac8c084174fc90a88ee27e2cd3b5
Nicholas Sim <nicholassimws@gmail.com>
2017-06-15 16:55:02 +0100

    notify.js: display notifications live

--- webapps34/intune/static/scripts/notify.js
+++ webapps34/intune/static/scripts/notify.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
     var notification_counter = $("#notification_counter");
+    var notification_menu = $("#notification-menu")
     var socket = new WebSocket("ws://" + window.location.host + "/notifications/");
     var notification_count = -1;
+    var received = false;
 
     socket.onmessage = function (e) {
         var data = JSON.parse(e.data);
@@ -13,18 +15,25 @@ $(document).ready(function () {
             }
         }
 
-        if (data["action"] == "unread_count") {
-            update_notification_count(data["unread"]);
-        } else {
-            update_notification_count(notification_count + 1);
-
-            // live updates on notifications page
-            var notif_msg = "<div class='notification col-sm-8'><p>" + data["msg"] + "</p></div>";
+        function prepend_notification(message, time, link) {
+            var notif_msg = "<div class='notification col-sm-8'><p>" + message + "</p></div>";
             var notif_time = "<div class='notification col-sm-4'><p> Just now </p></div>";
             var notif_li =
                 "<li><div class='composition-element notification-element'>" + notif_msg + notif_time + "</div></li>";
-            var notif_list = $("#notif-list");
-            notif_list.prepend(notif_li);
+            notification_menu.prepend(notif_li);
+        }
+
+        if (data["action"] == "unread_count") {
+            update_notification_count(data["unread"]);
+            console.log(data['notification_list'])
+            for (var i=0; i < data["notification_list"].length; i++) {
+                console.log("HEllo");
+                var notification = data["notification_list"][i];
+                prepend_notification(notification["msg"], notification["time"], notification["link"]);
+            }
+        } else {
+            update_notification_count(notification_count + 1);
+            prepend_notification(data["msg"], null, null);
         }
     };
 });
